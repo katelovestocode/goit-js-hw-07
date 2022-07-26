b9791b9dd8120466e4d6dbed2be884a98980051a
Kate Vaskevich <kate.panasiuk@yahoo.com>
2022-07-26 18:43:32 +0300

    Task# 2 and update to Task #1 with Esc key option

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -31,15 +31,42 @@ const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 // ADD EventListener on the click event
-galleryEl.addEventListener("click", onPictureContainerClick);
+galleryEl.addEventListener("click", onModalOpen);
 
-// Function that changes image src and uses basicLightbox modal window from the library
-function onPictureContainerClick(event) {
+// Declaring global variable that will be used in Modal Window
+let instance;
+
+// Function to check if the key that pressed is "Escape" key then close the Modal window if it is
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+
+  if (event.code === ESC_KEY_CODE) {
+    onModalClose();
+  }
+}
+
+// Function that closes the Modal Window
+function onModalClose() {
+  instance.close();
+}
+
+// Function that changes image src and uses basicLightbox to show Modal window
+function onModalOpen(event) {
   event.preventDefault(); // prevents a user to be redirected to open a picture
 
-  const instance = basicLightbox.create(
-    ` <img src = "${event.target.dataset.source}"/>`
+  instance = basicLightbox.create(
+    ` <img src = "${event.target.dataset.source}"/>`,
+    {
+      // instances if modal is open add EventLister
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      // instances if modal is closed remove EventLister
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
   );
-
+  // calling basicLightbox to show the modal window
   instance.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// CREATING a template render from the array for each image
+function createGalleryMarkup(items) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+
+  return markup;
+}
+
+// PUBLISHING a new markup/template with insertAdjacentHTML
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// ADD EventListener on the click event to the gallery div
+galleryEl.addEventListener("click", onPictureContainerClick);
+
+// Function that is calling SimpleLightbox with captions specified as options
+function onPictureContainerClick(event) {
+  event.preventDefault(); // prevents a user to be redirected to open a picture
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionType: "attr",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
